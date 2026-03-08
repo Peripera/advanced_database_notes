6f3523268299ee0e7bc6d687e8e3419f127964ee
Peripera <156480421+Peripera@users.noreply.github.com>
2026-03-08 13:24:10 -0600

    Completed challenges yay

--- sql_challenges/challenge-04/solution.sql
+++ sql_challenges/challenge-04/solution.sql
@@ -19,7 +19,7 @@ order  by shape, weight, brick_id;
 select b.brick_id, b.weight,
        round ( avg ( weight ) over ( -- redondear el promedio del peso de la primera fila por brick id de la primera a la ultima                                
          order by brick_id
-       ), 2 ) running_average_weight -- Declara atributo ruuning_average_weight para obvservar los resultadps  
+       ), 2 ) running_average_weight -- Declara atributo ruuning_average_weight para observar los resultadOs  
 from   bricks b
 order  by brick_id;
 
@@ -37,5 +37,45 @@ select b.*,
 from   bricks b
 order  by weight;
 
+-- Task 4}
 
+with totals as (
+  select b.*,
+         sum ( weight ) over (
+           partition by shape)
+         weight_per_shape,
+         sum ( weight ) over (
+           ORDER BY brick_id)
+        running_weight_by_id
+  from   bricks b
+)
+select * from totals
+where  weight_per_shape > 4 AND running_weight_by_id > 4
+order  by brick_id
+
+-- Data lemur FAANG question
+
+SELECT
+  D.department_name,
+  E.name,
+  E.salary
+FROM (
+  SELECT 
+    name,
+    salary,
+    department_id,
+    DENSE_RANK() OVER(PARTITION BY department_id ORDER BY salary DESC) AS salary_rank
+  FROM 
+    employee
+  ) AS e
+JOIN
+  department AS D
+ON
+  E.department_id = D.department_id
+WHERE
+  salary_rank <= 3
+ORDER BY 
+  department_name ASC,
+  salary DESC,
+  name ASC-,
 
